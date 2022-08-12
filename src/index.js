c328d63846e5e16760fd5091be22123cf24189e5
polonskiy041283 <polonskiy041283@gmail.com>
2022-08-13 01:26:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let result = '' + Math.abs(n);    
+    result = result.split('').reverse().join('');  
+    result = +result 
+    return result;
 }
